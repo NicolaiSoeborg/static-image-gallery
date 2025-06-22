271550a56d9e8e64770494107f53bc5a5967c615
Nicolai Søborg <8722223+NicolaiSoeborg@users.noreply.github.com>
2025-06-22 23:40:44 +0200

    Update sort-no-tsv.py

--- scripts/sort-no-tsv.py
+++ scripts/sort-no-tsv.py
@@ -5,19 +5,28 @@ from subprocess import PIPE, run
 def file_to_date(fname):
     exif = run(['jhead', fname], check=False, stdout=PIPE).stdout
     if not exif:
-        print(f"Has no EXIF: {fname}")
-        return
+        print(f"Not an image?: {fname}")
+        exif = run(['exiftool', fname], check=False, stdout=PIPE).stdout
+        if not exif:
+            return
+    
     exif = [line.split(": ", 1) for line in exif.decode().split("\n")]
     # Find this line: `Date/Time    : 2023:07:01 16:50:00`
-    exif = [kv[1] for kv in exif if 'Date/Time' in kv[0]]
-    if len(exif) != 1:
-        print(f'Exactly one line should be found in {fname}: {exif}')
+    ts = [kv[1] for kv in exif if 'Date/Time' in kv[0]]
+    if not ts:
+        # For "exiftool" output, we might find this instead:
+        # Create Date        : 2025:06:01 17:50:19
+        ts = [kv[1] for kv in exif if 'Create Date' in kv[0]]
+
+    if len(ts) != 1:
+        print(f'Exactly one timestamp should be found in {fname}: {ts}')
         return
-    elif exif[0] == "0000:00:00 00:00:00":
+    ts = ts[0]
+    elif ts == "0000:00:00 00:00:00":
         print(f"Bad EXIF date: {fname}")
         return
     else:
-        return datetime.strptime(exif[0], "%Y:%m:%d %H:%M:%S")
+        return datetime.strptime(ts, "%Y:%m:%d %H:%M:%S")
 
 def doit():
     for fname in glob("*"):
@@ -25,5 +34,5 @@ def doit():
         if ts:
             yield (fname, ts)
 
-for fname, ts in sorted(doit(), key=lambda pair: pair[1]):
+for fname, ts in sorted(doit(), key=lambda kv: kv[1]):
     print(f'{ts}: {fname}')
